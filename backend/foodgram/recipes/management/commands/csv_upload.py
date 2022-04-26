2b5f5e6b2ec4ebe2d5364f5e264327d14108cd01
simatheone <samone07@mail.ru>
2022-04-26 22:59:37 +0300

    Create management command

---
+++ backend/foodgram/recipes/management/commands/csv_upload.py
@@ -0,0 +1,26 @@
+import csv
+from pathlib import Path
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+from foodgram.settings import BASE_DIR
+
+def ingredient_create(row):
+    Ingredient.objects.get_or_create(
+        name=row[0],
+        measurement_unit=row[1]
+    )
+
+action = {
+    'ingredients.csv': ingredient_create
+}
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        path = str(BASE_DIR.parent.parent.joinpath('data').resolve()) + '/'
+        for key in action:
+            with open(path + key, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    action[key](row)
