abb74dcbfcdc64a901eef6646e68cd7af9c98dca
simatheone <samone07@mail.ru>
2022-04-26 22:07:31 +0300

    First migrations

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,110 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models import UniqueConstraint
+
+from foodgram.settings import MAX_LEN_REPR
 
 
-class Recipe(models.Model):
-    """Recipe model."""
-
-    author = models.ForeignKey(
-        CusomUser,
-        default='Deleted User',
-        on_delete=models.SET_DEFAULT
-    )
-    name = models.CharField(
-        'Название рецепта',
-        max_length=256,
-    )
-    image = models.ImageField(
-        'Картинка блюда',
-        upload_to='recipes/images/',
-    )
-    text = models.TextField(
-        'Текстовое описание рецепта'
-    )
-    ingredient = models.ManyToManyField(
-        Ingredient,
-        related_name='recipe_ingredient',
-        through=RecipeIngredient,
-        verbose_name='Ингредиент'
-    )
-    tag = models.ManyToManyFeld(
-        Tag,
-        related_name='recipe_tag',
-        through=RecipeTag,
-        verbose_name='Тэг'
-    )
-    cooking_time = models.PositiveSmallIntegerField(
-        verbose_name='Время приготовления'
-    )
-
-    class Meta:
-        db_table = 'recipe'
-        verbose_name = 'Рецепт'
-        verbose_name_plural = 'Рецепты'
-
-    def __str__(self):
-        return self.name[:30]
-
-
-class RecipeIngredient(models.Model):
-    """
-    Model though which the m2m relationship was established.
-    Chained models: Recipe, Ingredient.
-    """
-
-    recipe = models.ForeignKey(
-        Recipe,
-        related_name='recipe',
-        on_delete=models.???
-    )
-    ingredient = models.ForeignKey(
-        Ingredient,
-        related_name='ingredient',
-        on_delete=models.???
-    )
-
-    class Meta:
-        db_table='recipe_ingredient'
-        verbose_name = 'Рецепт-ингредиент'
-        constraints = [
-            UniqueConstraint(
-                fields=['recipe', 'ingredient'], name='unique_ingredient_for_recipe'
-            ),
-        ]
-
-    def __str__(self):
-        return (f'В рецепт: {self.recipe}' 
-               ' добавлен игредиент: {self.ingredient}')
-
-
-class RecipeTag(models.Model):
-    """
-    Model though which the m2m relationship was established.
-    Chained models: Recipe, Tag.
-    """
-
-    recipe = models.ForeignKey(
-        Recipe,
-        related_name='recipe',
-        on_delete=models.???
-    )
-    tag = models.ForeignKey(
-        Tag,
-        related_name='tag',
-        on_delete=models.???
-    )
-
-    class Meta:
-        db_table='recipe_tag'
-        verbose_name = 'Рецепт-тэг'
-        constraints = [
-            UniqueConstraint(
-                fields=['recipe', 'tag'], name='unique_tag_for_recipe'
-            ),
-        ]
-
-    def __str__(self):
-        return f'К рецепту: {self.recipe} добавлен тег: {self.tag}'
+CustomUser = get_user_model()
 
 
 class Tag(models.Model):
@@ -130,9 +31,14 @@ class Tag(models.Model):
         db_table = 'tag'
         verbose_name = 'Тэг'
         verbose_name_plural = 'Тэги'
+        indexes = [
+            models.Index(
+                fields=['name'], name='tag_name_idx'
+            )
+        ]
 
     def __str__(self):
-        return self.name[:30]
+        return self.name[:MAX_LEN_REPR]
 
 
 class Ingredient(models.Model):
@@ -154,9 +60,131 @@ class Ingredient(models.Model):
         db_table = 'ingredient'
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
+        indexes = [
+            models.Index(
+                fields=['name'], name='ingredient_name_idx'
+            )
+        ]
 
     def __str__(self):
-        return self.name[:30]
+        return self.name[:MAX_LEN_REPR]
+
+
+class Recipe(models.Model):
+    """Recipe model."""
+
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    name = models.CharField(
+        'Название рецепта',
+        max_length=200,
+    )
+    image = models.ImageField(
+        'Картинка блюда',
+        upload_to='recipes/images/',
+    )
+    text = models.TextField(
+        'Текстовое описание рецепта'
+    )
+    ingredient = models.ManyToManyField(
+        Ingredient,
+        related_name='recipe_ingredient',
+        through='RecipeIngredient',
+        verbose_name='Ингредиент'
+    )
+    tag = models.ManyToManyField(
+        Tag,
+        related_name='recipe_tag',
+        through='RecipeTag',
+        verbose_name='Тэг'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления'
+    )
+
+    class Meta:
+        db_table = 'recipe'
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        indexes = [
+            models.Index(
+                fields=['name'], name='recipe_name_idx'
+            ),
+            models.Index(
+                fields=['author'], name='recipe_author_idx'
+            )
+        ]
+
+    def __str__(self):
+        return self.name[:MAX_LEN_REPR]
+
+
+class RecipeIngredient(models.Model):
+    """
+    Model though which the m2m relationship was established.
+    Chained models: Recipe, Ingredient.
+    """
+
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='recipes',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        related_name='ingredients',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    class Meta:
+        db_table='recipe_ingredient'
+        verbose_name = 'Рецепт-ингредиент'
+        constraints = [
+            UniqueConstraint(
+                fields=['recipe', 'ingredient'], name='unique_ingredient_for_recipe'
+            ),
+        ]
+
+    def __str__(self):
+        return (f'В рецепт: {self.recipe}' 
+               ' добавлен игредиент: {self.ingredient}')
+
+
+class RecipeTag(models.Model):
+    """
+    Model though which the m2m relationship was established.
+    Chained models: Recipe, Tag.
+    """
+
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='recipe',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    tag = models.ForeignKey(
+        Tag,
+        related_name='tag',
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
+    class Meta:
+        db_table='recipe_tag'
+        verbose_name = 'Рецепт-тэг'
+        constraints = [
+            UniqueConstraint(
+                fields=['recipe', 'tag'], name='unique_tag_for_recipe'
+            ),
+        ]
+
+    def __str__(self):
+        return f'К рецепту: {self.recipe} добавлен тег: {self.tag}'
 
 
 class Favorite(models.Model):
@@ -165,12 +193,12 @@ class Favorite(models.Model):
     user = models.ForeignKey(
         CustomUser,
         related_name='favorite_user',
-        on_delete=models.???
+        on_delete=models.CASCADE
     )
     recipe = models.ForeignKey(
         Recipe,
-        related_name='favorite_recipe'
-        on_delete=models.???
+        related_name='favorite_recipe',
+        on_delete=models.CASCADE
     )
 
     class Meta:
@@ -187,36 +215,6 @@ class Favorite(models.Model):
         return f'{self.user} добавил(-а) в избранное {self.recipe}'
 
 
-class Subscription(models.Model):
-    """Subscription model."""
-
-    user = models.ForeignKey(
-        CusomUser,
-        related_name='sub_user',
-        on_delete=models.???
-    )
-    author = models.ForeignKey(
-        CustomUser,
-        related_name='sub_author',
-        on_delete=models.???
-    )
-
-    class Meta:
-        db_table='subscription'
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подписки'
-        constraints = [
-            UniqueConstraint(
-                fields=['user', 'author'], name='unique_subscription'
-            ),
-            CheckConstraint(check=~Q(user=F('author')),
-                            name='user_cant_follow_himself'),
-        ]
-
-    def __str__(self):
-        return f'{self.user} подписался на {self.author}'
-
-
 class ShoppingCart(models.Model):
     """
     Shopping Cart model.
@@ -224,14 +222,14 @@ class ShoppingCart(models.Model):
     """
 
     user = models.ForeignKey(
-        User,
+        CustomUser,
         related_name='cart_user',
-        on_delete=models.???
+        on_delete=models.CASCADE
     )
     recipe = models.ForeignKey(
         Recipe,
         related_name='cart_recipe',
-        on_delete=models.???
+        on_delete=models.CASCADE
     )
 
     class Meta:
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import CheckConstraint, F, UniqueConstraint, Q
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
+USER_ROLE_USER = 'user'
+USER_ROLE_ADMIN = 'admin'
+
+USER_ROLE_CHOICES = (
+    (USER_ROLE_USER, 'Пользователь'),
+    (USER_ROLE_ADMIN, 'Администратор')
+)
+
+
+class CustomUser(AbstractUser):
+    """CustomUser model."""
+
+    username = models.CharField(
+        'Юзернэйм',
+        max_length=150,
+        unique=True
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=254,
+        unique=True, 
+    )
+    role = models.CharField(
+        'Роль',
+        max_length=15,
+        choices=USER_ROLE_CHOICES,
+        default=USER_ROLE_USER
+    )
+
+    class Meta:
+        db_table = 'custom_user'
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('username', 'email'), name='unique_username_email'
+            )
+        ]
+
+    def __str__(self):
+        return f'Пользователь {self.username}'
+
+
+class Subscription(models.Model):
+    """Subscription model."""
+
+    user = models.ForeignKey(
+        CustomUser,
+        related_name='sub_user',
+        on_delete=CASCADE
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        related_name='sub_author',
+        on_delete=CASCADE
+    )
+
+    class Meta:
+        db_table='subscription'
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'author'], name='unique_subscription'
+            ),
+            CheckConstraint(check=~Q(user=F('author')),
+                            name='user_cant_follow_himself'),
+        ]
+
+    def __str__(self):
+        return f'{self.user} подписался на {self.author}'
