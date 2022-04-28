a91ef6111a9c8388ddf55fe419f8754af426731c
simatheone <samone07@mail.ru>
2022-04-29 00:02:51 +0300

    Configure routes to auth

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from .views import IngredientViewSet, TagViewSet
@@ -9,25 +9,8 @@ router_v1 = DefaultRouter()
 router_v1.register(r'tags', TagViewSet, basename='tags')
 router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
-
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path(r'', include('djoser.urls')),
-    path(r'^auth/', include('djoser.urls.authtoken'))
+    re_path(r'', include('djoser.urls')),
+    re_path(r'auth/', include('djoser.urls.authtoken'))
 ]
-
-
-
-"""
-AUTHORIZATION
-
-/api/users/ - get (list of users)       perms: ALL
-/api/users/ - post (registration of user)       perms: ALL
-
-/api/users/{id}/ - get (user profile)       perms: Authenticated - TOKEN
-/api/users/me/ - get (current user)       perms: Authenticated - TOKEN 
-
-/api/users/set_password/ - post (password change)       perms: Authenticated
-/api/auth/token/login/ - post (getting auth token)       perms: ALL
-/api/auth/token/logout/ - post (deletion of a token)       perms: Authenticated
-"""
