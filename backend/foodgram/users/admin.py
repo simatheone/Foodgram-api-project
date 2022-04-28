47c20e72cc1b3c398d8705d6c9ce59002a56e3fe
Alexandr Sviridov <78508673+Simatheone@users.noreply.github.com>
2022-04-28 14:37:26 +0300

    Register Sub model in admin panel

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from foodgram.settings import EMPTY_VALUE_ADMIN_PANEL
+from .models import CustomUser, Subscription
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+  list_display = ('user', 'author')
+  list_display_links = ('user',)
+  list_editable = ('author',)
+  list_filter = ('user',)
+  search_fields = ('user', 'author')
+  empty_value_display = EMPTY_VALUE_ADMIN_PANEL
