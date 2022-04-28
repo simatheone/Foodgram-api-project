900563077de86586ff2e4959ba81750e37054536
simatheone <samone07@mail.ru>
2022-04-28 20:18:35 +0300

    Register CustomUser in admin

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -12,3 +12,41 @@ class SubscriptionAdmin(admin.ModelAdmin):
   list_filter = ('user',)
   search_fields = ('user', 'author')
   empty_value_display = EMPTY_VALUE_ADMIN_PANEL
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+  list_display = (
+    'id',
+    'username',
+    'email',
+    'first_name',
+    'last_name',
+    'password',
+    'role',
+    'is_superuser',
+    'is_active',
+    'date_joined',
+    'is_staff'
+  )
+  list_editable = (
+    'username',
+    'email',
+    'first_name',
+    'last_name',
+    'password',
+    'role',
+    'is_superuser',
+    'is_active',
+    'is_staff'
+  )
+  list_filter = (
+    'username',
+    'email',
+    'first_name',
+    'last_name',
+    'role',
+    'date_joined',
+  )
+  search_fields = ('username', 'email', 'first_name')
+  empty_value_display = EMPTY_VALUE_ADMIN_PANEL
