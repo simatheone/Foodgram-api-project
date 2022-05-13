5bd122899525cddc1d48c3e0b56d5a2c85dda260
simatheone <samone07@mail.ru>
2022-05-13 23:42:34 +0300

    Make permissions and filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,19 +1,21 @@
 from django_filters import filters, FilterSet, widgets
 from django_filters.filters import (
-    CharFilter, BooleanFilter, ModelMultipleChoiceFilter, NumberFilter
+    BooleanFilter, CharFilter, ModelChoiceFilter,
+    ModelMultipleChoiceFilter, NumberFilter 
 )
 from recipes.models import Recipe, Ingredient, Tag
+from users.models import CustomUser
 
 
 class RecipeFilter(FilterSet):
     tags = ModelMultipleChoiceFilter(
-        name='tag__slug',
+        field_name='tags__slug',
         to_field_name='slug',
         lookup_type='in',
         queryset=Tag.objects.all()
     )
-    author = NumberFilter(
-        field_name='author__id', lookup_expr='iexact'
+    author = ModelChoiceFilter(
+        queryset=CustomUser.objects.all()
     )
     is_in_shopping_cart = BooleanFilter(
         widget=widgets.BooleanWidget(),
---
+++ backend/api/permissions.py
@@ -0,0 +1,39 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+from users.models import USER_ROLE_ADMIN
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """
+    Permission for admin, superuser.
+    Admin and superuser has permissions for reading/writing.
+    Users have permissions for reading.
+    """
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or (
+            request.user.is_authenticated
+            and (request.user.is_superuser
+            or reuqest.user.role == USER_ROLE_ADMIN)
+        )
+
+class IsOwnerAdminOrReadOnly(BasePermission):
+    """
+    Permission for admin, superuser, owner.
+    Admin and superuser have permissions for writing.
+    """
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+            or request.user.role == USER_ROLE_ADMIN
+            or request.user.is_superuser
+        )
+
+class IsAdminOrOwner(BasePermission):
+    pass
