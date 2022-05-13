db65a9ccc4bc7e28521c9b9400de11c5f532a4df
simatheone <samone07@mail.ru>
2022-05-13 23:45:40 +0300

    Make basic settings for pdf download

--- backend/api/views.py
+++ backend/api/views.py
@@ -185,7 +185,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         recipes_list = user.shopping_cart.all()
         ingredients_list = shopping_cart_dict(recipes_list)
-        print(ingredients_list)
 
         if recipes_list:
             ingredients = []
@@ -215,39 +214,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             filename=f'{user.username}\'s {SHOPPING_CART_FILENAME}'
         )
 
-        # x_position, y_position = 50, 800
-        # shopping_cart = (
-        #     request.user.shopping_cart.recipe.
-        #     values(
-        #         'ingredients__name',
-        #         'ingredients__measurement_unit'
-        #     ).annotate(amount=Sum('recipe__amount')).order_by())
-        # page.setFont('Vera', 14)
-        # if shopping_cart:
-        #     indent = 20
-        #     page.drawString(x_position, y_position, 'Cписок покупок:')
-        #     for index, recipe in enumerate(shopping_cart, start=1):
-        #         page.drawString(
-        #             x_position, y_position - indent,
-        #             f'{index}. {recipe["ingredients__name"]} - '
-        #             f'{recipe["amount"]} '
-        #             f'{recipe["ingredients__measurement_unit"]}.')
-        #         y_position -= 15
-        #         if y_position <= 50:
-        #             page.showPage()
-        #             y_position = 800
-        #     page.save()
-        #     buffer.seek(0)
-        #     return FileResponse(
-        #         buffer, as_attachment=True, filename=FILENAME)
-        # page.setFont('Vera', 24)
-        # page.drawString(
-        #     x_position,
-        #     y_position,
-        #     'Cписок покупок пуст!')
-        # page.save()
-        # buffer.seek(0)
-        # return FileResponse(buffer, as_attachment=True, filename=FILENAME)
+       
 
 class FavoriteViewSet(CreateDestroyViewSet):
     serializer_class = ShortRecipeSerializer
