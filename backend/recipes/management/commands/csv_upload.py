1a6c14544eb2f8383e543aebe62a2f3438c9942d
simatheone <samone07@mail.ru>
2022-05-14 19:51:40 +0300

    Fix typos

--- backend/recipes/management/commands/csv_upload.py
+++ backend/recipes/management/commands/csv_upload.py
@@ -1,9 +1,9 @@
 import csv
-from pathlib import Path
 
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 from foodgram.settings import BASE_DIR
+
 
 def ingredient_create(row):
     Ingredient.objects.get_or_create(
@@ -11,9 +11,11 @@ def ingredient_create(row):
         measurement_unit=row[1]
     )
 
+
 action = {
     'ingredients.csv': ingredient_create
 }
+
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
