69fde8372adcd9c6b019d9cbf96d90d75fc000f6
simatheone <samone07@mail.ru>
2022-05-14 19:51:20 +0300

    Add filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,9 +1,7 @@
-from django_filters import filters, FilterSet, widgets
-from django_filters.filters import (
-    BooleanFilter, CharFilter, ModelChoiceFilter,
-    ModelMultipleChoiceFilter, NumberFilter 
-)
-from recipes.models import Recipe, Ingredient, Tag
+from django_filters import FilterSet
+from django_filters.filters import (CharFilter, ModelChoiceFilter,
+                                    ModelMultipleChoiceFilter, NumberFilter)
+from recipes.models import Ingredient, Recipe, Tag
 from users.models import CustomUser
 
 
@@ -11,20 +9,37 @@ class RecipeFilter(FilterSet):
     tags = ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
-        lookup_type='in',
         queryset=Tag.objects.all()
     )
     author = ModelChoiceFilter(
         queryset=CustomUser.objects.all()
     )
-    is_in_shopping_cart = BooleanFilter(
-        widget=widgets.BooleanWidget(),
-        label='in shopping cart'
+    is_favorited = NumberFilter(
+        method='filter_is_favorited'
     )
-    is_favorited = BooleanFilter(
-        widget=widgets.BooleanWidget(),
-        label='in favorite'
+    is_in_shopping_cart = NumberFilter(
+        method='filter_is_in_shopping_cart'
     )
+
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value == 1:
+            queryset = Recipe.objects.filter(favorite_recipe__user=user)
+        if value == 0:
+            queryset = Recipe.objects.all().exclude(
+                favorite_recipe__user=user
+            )
+        return queryset.order_by('-pk')
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value == 1:
+            queryset = Recipe.objects.filter(shopping_cart__user=user)
+        if value == 0:
+            queryset = Recipe.objects.all().exclude(
+                shopping_cart__user=user
+            )
+        return queryset.order_by('-pk')
 
     class Meta:
         model = Recipe
@@ -32,7 +47,7 @@ class RecipeFilter(FilterSet):
 
 
 class IngredientFilter(FilterSet):
-    name = CharFilter(lookup_expr='istartswith')
+    name = CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
         model = Ingredient
