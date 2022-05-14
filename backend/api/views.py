963fbcc2ee767ce0f721f70a6fd5fa1f855d7f9c
simatheone <samone07@mail.ru>
2022-05-14 19:52:43 +0300

    Polish code

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,50 +1,36 @@
 import io
 
+from django.db.models import Value
 from django.http import FileResponse
-from django.db.models import Value, Sum, Count, Exists
-from djoser.views import UserViewSet
 from django.shortcuts import get_object_or_404
-
-from reportlab.pdfgen import canvas
+from djoser.views import UserViewSet
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.lib.styles import getSampleStyleSheet
-
+from reportlab.pdfgen import canvas
+from rest_framework import generics, status, viewsets
 from rest_framework.decorators import action
-from rest_framework import viewsets, views, status, mixins, generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.filters import SearchFilter
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
-from rest_framework.decorators import action
-from rest_framework.pagination import LimitOffsetPagination
 from django_filters.rest_framework import DjangoFilterBackend
 
 from foodgram.settings import SHOPPING_CART_FILENAME
-from recipes.models import(
-    Favorite, Ingredient, Recipe,
-    RecipeIngredientAmount, ShoppingCart, Tag
-)
+from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 from users.models import CustomUser, Subscription
-from .serializers import (
-    CustomUserSerializer, IngredientSerializer, TagSerializer,
-    RecipeReadSerializer, RecipeWriteSerializer,
-    SubscriptionSerializer, ShortRecipeSerializer
-)
-from .filters import RecipeFilter, IngredientFilter
+from .filters import IngredientFilter, RecipeFilter
 from .permissions import IsAdminOrReadOnly, IsOwnerAdminOrReadOnly
-from .utils import shopping_cart_dict
+from .serializers import (CustomUserSerializer, IngredientSerializer,
+                          RecipeReadSerializer, RecipeWriteSerializer,
+                          ShortRecipeSerializer, SubscriptionSerializer,
+                          TagSerializer)
+from .utils import get_shopping_cart_for_writing
 
 SELF_FOLLOWING_ERROR = 'Пользователь не может подписаться сам на себя.'
 DOUBLE_FOLLOWING_ERROR = 'Нельзя дважды подписаться на одного юзера.'
 DOUBLE_FAVORITE_ERROR = 'Нельзя дважды добавить рецепт в избранное.'
-
-class CreateDestroyViewSet(
-    mixins.CreateModelMixin,
-    mixins.DestroyModelMixin,
-    viewsets.GenericViewSet
-):
-    pass
+DOUBLE_SHOPPING_ERROR = 'Нельзя добавить в покупки два одинаковых рецепта.'
+CART_TITLE = 'СПИСОК ПОКУПОК'
+EMPTY_CART_TITLE = 'Список покупок пуст'
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
@@ -58,36 +44,25 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     """IngredientViewSet for listing and retrieving Ingredients."""
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    filter_class = IngredientFilter
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = IngredientFilter
     permission_classes = (IsAdminOrReadOnly,)
 
 
 class CustomUserViewSet(UserViewSet):
     """
     The viewset for model CustomUser.
-    Available endpoints with: 
-        GET method: 
+    Available endpoints with:
+        GET method:
             api/users/, api/users/{user_id}/,
             api/users/me/, api/users/subscriptions/
         POST method:
             api/users/, api/users/set_password/,
             api/auth/token/login/, api/auth/token/logout/
-            api/users/{user_id}/subscribe/
-        DELETE method:
-            api/users/{user_id}/subscribe/
+    Permissions for users are set in Djoser library.
     """
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = (AllowAny,) # от анонима пост запрос на регистрацию ????
-
-    def get_queryset(self):
-        user = self.request.user
-        if self.action in ('subscriptions', 'subscribe'):
-            queryset = user.sub_user.annotate(
-                is_subscribed=Value(True)
-            )
-            return queryset
-        return super().get_queryset()
 
     @action(
         detail=False,
@@ -95,8 +70,10 @@ class CustomUserViewSet(UserViewSet):
         url_name='subscriptions'
     )
     def subscriptions(self, request):
-        user = request.user
-        queryset = self.get_queryset()
+        user = self.request.user
+        queryset = user.sub_user.annotate(
+                is_subscribed=Value(True)
+            )
         serializer = SubscriptionSerializer(
             queryset, many=True, context={'request': request}
         )
@@ -105,6 +82,7 @@ class CustomUserViewSet(UserViewSet):
 
 class SubscribeAPIView(generics.ListCreateAPIView,
                        generics.DestroyAPIView):
+    """SubscribeAPIView for Subscription model."""
     serializer_class = SubscriptionSerializer
     permission_classes = (IsAuthenticated,)
 
@@ -113,7 +91,7 @@ class SubscribeAPIView(generics.ListCreateAPIView,
         queryset = get_object_or_404(CustomUser, pk=author_id)
         return queryset
 
-    def create(self, request, *args, **kwargs):  
+    def create(self, request, *args, **kwargs):
         user = request.user
         author_obj = self.get_queryset()
         if user == author_obj:
@@ -122,12 +100,13 @@ class SubscribeAPIView(generics.ListCreateAPIView,
                 status=status.HTTP_400_BAD_REQUEST
             )
         if user.sub_user.filter(author=author_obj).exists():
-            message = 'Нельзя дважды подписаться на одного юзера.'
             return Response(
                 {'message': DOUBLE_FOLLOWING_ERROR},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        subscription = Subscription.objects.create(user=user, author=author_obj)
+        subscription = Subscription.objects.create(
+            user=user, author=author_obj
+        )
         serializer = self.get_serializer(
             subscription, context={'request': request}
         )
@@ -144,8 +123,10 @@ class SubscribeAPIView(generics.ListCreateAPIView,
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
+    """RecipeViewSet for Recipe model."""
     queryset = Recipe.objects.all()
     permission_classes = (IsOwnerAdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
 
     def get_serializer_class(self):
@@ -162,7 +143,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(
         detail=False,
         methods=['get'],
-        permission_classes=(AllowAny,),
+        permission_classes=(IsAuthenticated,),
         url_name='download_recipe'
     )
     def download_shopping_cart(self, request):
@@ -171,30 +152,34 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """
         user = request.user
         buffer = io.BytesIO()
-        styles = getSampleStyleSheet()
         pdf_page = canvas.Canvas(buffer, pagesize=letter)
 
         # Set fonts for pdf file
-        pdfmetrics.registerFont(TTFont('DejaVuSerif', 'DejaVuSerif.ttf', 'UTF-8'))
-        pdf_page.setFont('DejaVuSerif', 16)
+        pdfmetrics.registerFont(TTFont(
+            'DejaVuSerif', 'DejaVuSerif.ttf', 'UTF-8'
+            )
+        )
+        pdf_page.setFont('DejaVuSerif', 14)
 
         # Set x and y positions for the text on the page
-        # Set the indent size for the text
-        x_value , y_value = 315, 735
-        indent_size = 15
+        x_value, y_value = 20, 600
 
         recipes_list = user.shopping_cart.all()
-        ingredients_list = shopping_cart_dict(recipes_list)
+        # Calling a function from utils
+        ingredients_list = get_shopping_cart_for_writing(recipes_list)
 
         if recipes_list:
-            ingredients = []
-            pdf_page.drawCentredString(315, 750, 'Список покупок')
+            pdf_page.drawCentredString(315, 700, CART_TITLE)
 
-            # for value in ingredients:
-            #     pdf_page.drawString(
-            #         x_value - indent_size, y_value, value
-            #     )
-            #     y_value -= 20
+            for value in ingredients_list:
+                name = value.capitalize()
+                amount = ingredients_list[value]['amount']
+                measure = ingredients_list[value]['measurement_unit']
+                write_string = f'{name} - {amount} ({measure});'
+                pdf_page.drawString(
+                    x_value, y_value, write_string
+                )
+                y_value -= 30
             pdf_page.save()
             buffer.seek(0)
             return FileResponse(
@@ -202,32 +187,32 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 as_attachment=True,
                 filename=f'{user.username}\'s {SHOPPING_CART_FILENAME}'
             )
-            
+        else:
+            pdf_page.drawCentredString(315, 425, EMPTY_CART_TITLE)
+            pdf_page.showPage()
+            pdf_page.save()
+            buffer.seek(0)
+            return FileResponse(
+                buffer,
+                as_attachment=True,
+                filename=f'{user.username}\'s {SHOPPING_CART_FILENAME}'
+            )
 
-        pdf_page.drawCentredString(315, 425, 'Список покупок пуст')
-        pdf_page.showPage()
-        pdf_page.save()
-        buffer.seek(0)
-        return FileResponse(
-            buffer,
-            as_attachment=True,
-            filename=f'{user.username}\'s {SHOPPING_CART_FILENAME}'
-        )
 
-       
-
-class FavoriteViewSet(CreateDestroyViewSet):
+class FavoriteAPIView(generics.ListCreateAPIView,
+                      generics.DestroyAPIView):
+    """FavoriteApiView for Favorite model."""
     serializer_class = ShortRecipeSerializer
     permission_classes = (IsAuthenticated,)
 
-    def get_object(self):
+    def get_queryset(self):
         recipe_id = self.kwargs['recipe_id']
         recipe = get_object_or_404(Recipe, pk=recipe_id)
         self.check_object_permissions(self.request, recipe)
         return recipe
 
     def create(self, request, *args, **kwargs):
-        recipe_obj = self.get_object()
+        recipe_obj = self.get_queryset()
         user = request.user
         if Favorite.objects.filter(user=user, recipe=recipe_obj).exists():
             return Response(
@@ -235,13 +220,15 @@ class FavoriteViewSet(CreateDestroyViewSet):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        serializer = self.get_serializer(recipe_ob, context={'request': request})
+        serializer = self.get_serializer(
+            recipe_obj, context={'request': request}
+        )
         Favorite.objects.create(user=user, recipe=recipe_obj)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def delete(self, request, *args, **kwargs):
         user = request.user
-        recipe_obj = self.get_object()
+        recipe_obj = self.get_queryset()
         favorite = Favorite.objects.filter(user=user, recipe=recipe_obj)
         if not favorite:
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -249,22 +236,26 @@ class FavoriteViewSet(CreateDestroyViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ShoppingCartViewSet(CreateDestroyViewSet):
+class ShoppingCartAPIView(generics.ListCreateAPIView,
+                          generics.DestroyAPIView):
+    """Shopping cart ApiView for ShoppingCart model."""
     serializer_class = ShortRecipeSerializer
     permission_classes = (IsAuthenticated,)
 
-    def get_object(self):
+    def get_queryset(self):
         recipe_id = self.kwargs['recipe_id']
         recipe = get_object_or_404(Recipe, pk=recipe_id)
         self.check_object_permissions(self.request, recipe)
         return recipe
 
     def create(self, request, *args, **kwargs):
-        recipe_obj = self.get_object()
+        recipe_obj = self.get_queryset()
         user = request.user
         if ShoppingCart.objects.filter(user=user, recipe=recipe_obj).exists():
-            message = 'Нельзя добавить в покупки два одинаковых рецепта.'
-            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {'message': DOUBLE_SHOPPING_ERROR},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         serializer = self.get_serializer(recipe_obj)
         ShoppingCart.objects.create(user=user, recipe=recipe_obj)
@@ -272,7 +263,7 @@ class ShoppingCartViewSet(CreateDestroyViewSet):
 
     def delete(self, request, *args, **kwargs):
         user = request.user
-        recipe_obj = self.get_object()
+        recipe_obj = self.get_queryset()
         shopping_cart = Favorite.objects.filter(user=user, recipe=recipe_obj)
         if not shopping_cart:
             return Response(status=status.HTTP_400_BAD_REQUEST)
