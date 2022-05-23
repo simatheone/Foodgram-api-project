68dcfd56db643cd978e3f939e66aba0a04f39d43
simatheone <samone07@mail.ru>
2022-05-23 22:53:08 +0300

    Delete useless imports

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,4 @@
-from pathlib import Path
-
 from django.urls import include, path, re_path
-from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from .views import (CustomUserViewSet, FavoriteAPIView, IngredientViewSet,
