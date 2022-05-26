f7a03e8024e9111bc28493e0c5f7a8d06ad08616
simatheone <samone07@mail.ru>
2022-05-26 20:21:47 +0300

    Refactor utils.py

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,5 +1,7 @@
 import io
+from pathlib import Path
 
+from django.db.models import Sum
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
@@ -9,60 +11,7 @@ from recipes.models import RecipeIngredientAmount
 
 CART_TITLE = 'СПИСОК ПОКУПОК'
 EMPTY_CART_TITLE = 'Список покупок пуст'
-
-
-def get_shopping_cart_for_writing(recipes_list):
-    """
-    Function returns dictionary with prepared ingredients.
-    All ingredients are unique.
-    Dictionary schema: {
-        'ingredient_name': {
-            'amount': int amount,
-            'measurement_unit': str unit
-        }
-    }
-    """
-    ingredient_list = get_ingredients(recipes_list)
-    ingredients_for_repr = {}
-    for ingr in ingredient_list:
-        if ingr[0] in ingredients_for_repr and (
-           ingredients_for_repr[ingr[0]]['measurement_unit'] == ingr[2]
-           ):
-            ingredients_for_repr[ingr[0]]['amount'] += ingr[1]
-        else:
-            ingredients_for_repr[ingr[0]] = {
-                'amount': ingr[1],
-                'measurement_unit': ingr[2]
-            }
-    return ingredients_for_repr
-
-
-def get_ingredients(recipes_list):
-    """
-    Function returns a list with lists of ingredients.
-    Some ingredients could be repeated.
-    List schema example:
-        [
-            [str: potato, int: 2, str: kg.],
-            [str: lemon, int: 2, str: pieces],
-            ...
-            ...
-        ]
-    """
-    ingredient_list = []
-    for values in recipes_list:
-        recipe_obj = values.recipe
-        ingredient_obj = RecipeIngredientAmount.objects.filter(
-            recipe=recipe_obj).order_by()
-        for ingredient in ingredient_obj:
-            ingredient_list.append(
-                [
-                    ingredient.ingredient.name,
-                    ingredient.amount,
-                    ingredient.ingredient.measurement_unit
-                ]
-            )
-    return ingredient_list
+FONTS_DIR = Path('./static/fonts/DejaVuSerif.ttf').resolve()
 
 
 def create_pdf_shopping_cart(user):
@@ -77,23 +26,27 @@ def create_pdf_shopping_cart(user):
     pdf_page = canvas.Canvas(buffer, pagesize=letter)
 
     # Set fonts for pdf file
-    pdfmetrics.registerFont(TTFont('Vera', 'Vera.ttf'))
-    pdf_page.setFont('Vera', 14)
+    pdfmetrics.registerFont(TTFont('DejaVuSerif', FONTS_DIR))
+    pdf_page.setFont('DejaVuSerif', 14)
 
     # Set x and y positions for the text on the page
-    x_value, y_value = 20, 600
+    x_value, y_value = 20, 635
 
-    recipes_list = user.shopping_cart.all()
-    # Calling a function from utils
-    ingredients_list = get_shopping_cart_for_writing(recipes_list)
+    # Get all ingredients with it's name, amount (unique), measurement_unit
+    # Which are in request user's shopping cart
+    ingredients_list = RecipeIngredientAmount.objects.filter(
+        recipe__shopping_cart__user=user
+    ).values('ingredient__name', 'ingredient__measurement_unit').annotate(
+        total_amount=Sum('amount')
+    )
 
-    if recipes_list:
+    if ingredients_list:
         pdf_page.drawCentredString(315, 700, CART_TITLE)
 
         for value in ingredients_list:
-            name = value.capitalize()
-            amount = ingredients_list[value]['amount']
-            measure = ingredients_list[value]['measurement_unit']
+            name = value['ingredient__name'].capitalize()
+            amount = value['total_amount']
+            measure = value['ingredient__measurement_unit']
             write_string = f'{name} - {amount} ({measure});'
             pdf_page.drawString(
                 x_value, y_value, write_string
